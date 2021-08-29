9a396447e0f9245f065040ba8415946f306cd975
Thomas Liennard <thomas.liennard@thomas-liennard.fr>
2021-08-29 11:58:30 +0200

    fix sequences importer to not require empty sequences dir at root

--- goldobot/sequences_importer.py
+++ goldobot/sequences_importer.py
@@ -23,6 +23,12 @@ class MyMetaFinder(MetaPathFinder):
         invalidate_caches()        
         
     def find_spec(self, fullname, path, target=None):
+        if fullname == 'sequences':
+            file_location = Path(__file__) / Path('../sequences/__init__.py')
+            return spec_from_file_location(
+                fullname,
+                str(file_location),
+                loader=MyLoader(self, fullname, str(file_location)))
         if fullname.startswith('sequences.'):
             file_location = self.sequences_path / Path(fullname[10:] + '.py')
             return spec_from_file_location(
