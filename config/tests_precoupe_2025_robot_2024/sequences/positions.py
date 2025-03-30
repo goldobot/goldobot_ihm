f66314ed3b2160973c946def89bcc8f2f67bad7a
goldo31337 <echelon@free.fr>
2025-03-30 17:24:10 +0200

    * WIP : poses pour Belgique 2025

--- config/tests_precoupe_2025_robot_2024/sequences/positions.py
+++ config/tests_precoupe_2025_robot_2024/sequences/positions.py
@@ -7,6 +7,9 @@ def symetrie(pose):
             return np.array([pose[0], -pose[1]])
     return (pose[0], -pose[1], -pose[2])
 
+def symetrie_point(point):
+    return (point[0], -point[1])
+
 
 class Side:
     Unknown = 0
@@ -14,16 +17,113 @@ class Side:
     Blue = 2
 
 
-class YellowPoses:
-    ZoneA_J_start_pose   = (  0.30, -1.15,   0)
-    ZoneDA_J_start_pose  = (  1.80, -0.30, 180)
-    ZoneDL_J_start_pose  = (  1.10,  1.20, -90)
+ResourceCenter = {
+    "ZoneM1G" : ( 0.275, -0.675),
+    "ZoneM1D" : ( 0.275,  0.675),
+    "ZoneM2G" : ( 1.050, -0.400),
+    "ZoneM2D" : ( 1.050,  0.400),
+    "ZoneM3G" : ( 1.750, -0.725),
+    "ZoneM3D" : ( 1.750,  0.725),
+    "ZoneM4G" : ( 1.600, -1.425),
+    "ZoneM4D" : ( 1.600,  1.425),
+    "ZoneM5G" : ( 0.675, -1.425),
+    "ZoneM5D" : ( 0.675,  1.425),
+    }
 
+class YellowPoses:
+    # Reperes
+    ZoneA_center       = (  0.225, -1.125)
+    ZoneDA_center      = (  1.775, -0.275)
+    ZoneDL_center      = (  1.125,  1.275)
+    ZoneCA_center      = (  1.925, -0.725)
+    ZoneCC_center      = (  1.925,  1.275)
+
+    # Poses de depart
+    ZoneA_start_pose   = (  0.300, -1.150,   0)
+    ZoneDA_start_pose  = (  1.800, -0.300, 180)
+    ZoneDL_start_pose  = (  1.100,  1.200, -90)
+
+    # Action1
+    Act1_start         = ZoneDA_start_pose
+    Act1_preprise      = (  1.250, -0.400,   0)
+    Act1_predepose     = (  1.800, -0.300, 180)
+    Act1_traj1_start   = Act1_start
+    Act1_traj1_wp1     = (  1.500, -0.350, 180)
+    Act1_traj1_finish  = Act1_preprise
+    Act1_traj2_start   = Act1_preprise
+    Act1_traj2_wp1     = (  1.500, -0.350,   0)
+    Act1_traj2_finish  = Act1_predepose
+
+    # Action2
+    Act2_start         = Act1_predepose
+    Act2_preprise      = (  1.550, -0.725, 180)
+    Act2_predepose     = (  1.800, -0.725, 180)
+    Act2_traj1_start   = Act2_start
+    Act2_traj1_wp1     = (  1.550, -0.300, -90)
+    Act2_traj1_finish  = Act2_preprise
+
+    # Action3
+    Act3_start         = Act2_predepose
+    Act3_preprise      = (  1.600, -1.225,  90)
+    Act3_predepose     = (  1.600, -0.300, 180)
+    Act3_traj1_start   = Act3_start
+    Act3_traj1_wp1     = (  1.600, -0.725, -90)
+    Act3_traj1_finish  = Act3_preprise
+
+    # Action4
+    Act4_start         = Act3_predepose
+    Act4_traj1_start   = Act4_start
+    Act4_traj1_wp1     = (  1.100, -0.300,-135)
+    Act4_traj1_wp2     = (  0.750, -0.800, -90)
+    Act4_traj1_finish  = (  0.750, -1.200, -90)
+    Act4_final         = (  0.450, -1.200, 180)
 
 class BluePoses:
-    ZoneA_B_start_pose   = (  0.30,  1.15,   0)
-    ZoneDA_B_start_pose  = (  1.80,  0.30, 180)
-    ZoneDL_B_start_pose  = (  1.10, -1.20,  90)
+    # Reperes
+    ZoneA_center       = symetrie_point(YellowPoses.ZoneA_center)
+    ZoneDA_center      = symetrie_point(YellowPoses.ZoneDA_center)
+    ZoneDL_center      = symetrie_point(YellowPoses.ZoneDL_center)
+    ZoneCA_center      = symetrie_point(YellowPoses.ZoneCA_center)
+    ZoneCC_center      = symetrie_point(YellowPoses.ZoneCC_center)
+
+    ZoneA_start_pose   = symetrie(YellowPoses.ZoneA_start_pose)
+    ZoneDA_start_pose  = symetrie(YellowPoses.ZoneDA_start_pose)
+    ZoneDL_start_pose  = symetrie(YellowPoses.ZoneDL_start_pose)
+
+    # Action1
+    Act1_start         = symetrie(YellowPoses.Act1_start)
+    Act1_preprise      = symetrie(YellowPoses.Act1_preprise)
+    Act1_predepose     = symetrie(YellowPoses.Act1_predepose)
+    Act1_traj1_start   = symetrie(YellowPoses.Act1_traj1_start)
+    Act1_traj1_wp1     = symetrie(YellowPoses.Act1_traj1_wp1)
+    Act1_traj1_finish  = symetrie(YellowPoses.Act1_traj1_finish)
+    Act1_traj2_start   = symetrie(YellowPoses.Act1_traj2_start)
+    Act1_traj2_wp1     = symetrie(YellowPoses.Act1_traj2_wp1)
+    Act1_traj2_finish    = symetrie(YellowPoses.Act1_traj2_finish)
+
+    # Action2
+    Act2_start         = symetrie(YellowPoses.Act2_start)
+    Act2_preprise      = symetrie(YellowPoses.Act2_preprise)
+    Act2_predepose     = symetrie(YellowPoses.Act2_predepose)
+    Act2_traj1_start   = symetrie(YellowPoses.Act2_traj1_start)
+    Act2_traj1_wp1     = symetrie(YellowPoses.Act2_traj1_wp1)
+    Act2_traj1_finish  = symetrie(YellowPoses.Act2_traj1_finish)
+
+    # Action3
+    Act3_start         = symetrie(YellowPoses.Act3_start)
+    Act3_preprise      = symetrie(YellowPoses.Act3_preprise)
+    Act3_predepose     = symetrie(YellowPoses.Act3_predepose)
+    Act3_traj1_start   = symetrie(YellowPoses.Act3_traj1_start)
+    Act3_traj1_wp1     = symetrie(YellowPoses.Act3_traj1_wp1)
+    Act3_traj1_finish  = symetrie(YellowPoses.Act3_traj1_finish)
+
+    # Action4
+    Act4_start         = symetrie(YellowPoses.Act4_start)
+    Act4_traj1_start   = symetrie(YellowPoses.Act4_traj1_start)
+    Act4_traj1_wp1     = symetrie(YellowPoses.Act4_traj1_wp1)
+    Act4_traj1_wp2     = symetrie(YellowPoses.Act4_traj1_wp2)
+    Act4_traj1_finish  = symetrie(YellowPoses.Act4_traj1_finish)
+    Act4_final         = symetrie(YellowPoses.Act4_final)
 
 
 @robot.sequence
