550b580cbdf1fedceb5d77542117c21c3ccfbf81
goldo31337 <echelon@free.fr>
2026-04-17 15:46:25 +0200

    * WIP : prep. Coupe Be  - add rudimentary aruco detection in strat.

--- config/tests_precoupe_2026/sequences/sequences.py
+++ config/tests_precoupe_2026/sequences/sequences.py
@@ -6,6 +6,10 @@ import numpy as np
 
 import time
 
+# NOTE : prise_offset_back_robot = 0.305
+# NOTE : robot_back_length       = 0.084
+# NOTE : prise_offset            = 0.220
+
 # import modules from sequence directory
 from . import positions as pos
 from . import recalages
@@ -17,6 +21,8 @@ from . import robot_config as rc
 
 global_long_speed = 0.4
 global_turn_speed = 2.0
+
+aruco_start_configuration = "0000"
 
 @robot.sequence
 async def prematch():
@@ -67,6 +73,8 @@ async def start_match():
     Sequence called at the start of the match, before trying any action.
     This will typically be used to setup actuators and get out of the starting area.
     """
+
+    global aruco_start_configuration
 
     T0 = time.time()
 
@@ -127,4 +135,58 @@ async def start_match():
     print ("T match_timer = {}".format(T1-T0))
     print ("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
 
+
+@robot.sequence
+async def aruco_start_detection():
+    global aruco_start_configuration
+
+    #try:
+    detections = []
+    with open("/tmp/detections.txt") as f:
+        for line in f:
+            line = line.strip()
+            parts = line.split()
+            if len(parts) != 4:
+                continue
+            ts_str, my_id_str, x_str, y_str = parts
+            ts = float(ts_str)
+            my_id = int(my_id_str)
+            x_real = float(x_str)/1000.0
+            y_real = float(y_str)/1000.0
+            detections.append((ts,my_id,x_real,y_real))
+    #except:
+    #    print ("Cannot read (and parse) /tmp/detections.txt")
+    #    return
+
+    detections.sort(key=lambda d: d[2])
+
+    print()
+    print()
+    for i in range(len(detections)):
+        ts, my_id, x_real, y_real = detections[i]
+        print ("ts={:.2f} id={:d} x={:6.3f} y={:6.3f}".format(ts, my_id, x_real, y_real))
+    print()
+    print()
+
+    if (len(detections)<4):
+        print("Cannot detect aruco_start_configuration!")
+        return
+
+    if (robot.start_zone==1):   # BLUE   (Y+,id=36)
+        print("my_color = BLUE (id=36)")
+        my_color = 36
+    elif (robot.start_zone==2): # YELLOW (Y-,id=47)
+        print("my_color = YELLOW (id=47)")
+        my_color = 47
+        pass
+    else:
+        print("No start zone!")
+        return
+
+    aruco_start_configuration = "0000"
+    for i in range(0,4):
+        if (detections[i][1]==my_color):
+            aruco_start_configuration = aruco_start_configuration[:i] + '1' + aruco_start_configuration[i+1:]
+
+    print("aruco_start_configuration = {}".format(aruco_start_configuration))
 
