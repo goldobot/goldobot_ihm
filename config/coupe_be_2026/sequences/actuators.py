ecdeda31e7e8643d7c9a37be7845910349b569c1
Goldorak <echelon@free.fr>
2026-04-19 00:30:41 +0200

    * WIP : Coupe Be : first ful sequence

---
+++ config/coupe_be_2026/sequences/actuators.py
@@ -0,0 +1,402 @@
+import asyncio
+
+
+class GoldoLift:
+    Unknown = 0
+    Left = 1
+    Right = 2
+
+
+async def goldo_lift_move(side,pos):
+    lift_factor = 1.090
+    if (side==GoldoLift.Left):
+        await servos.liftsRaw(int(pos*lift_factor), 80, 0, 0)
+    if (side==GoldoLift.Right):
+        await servos.liftsRaw(0, 0, int(pos*lift_factor), 80)
+
+
+async def goldo_lifts_move(pos, speed = 80):
+    lift_factor = 1.090
+    await servos.liftsRaw(int(pos*lift_factor), speed, int(pos*lift_factor), speed)
+
+
+async def pump_on(left, right):
+    if left:
+        await robot.gpioSet('pompe_g', True)
+        await robot.gpioSet('pompe_g', True)
+    if right:
+        await robot.gpioSet('pompe_d', True)
+        await robot.gpioSet('pompe_d', True)
+
+
+async def pump_off(left, right):
+    if left:
+        await robot.gpioSet('pompe_g', False)
+        await robot.gpioSet('pompe_g', False)
+    if right:
+        await robot.gpioSet('pompe_d', False)
+        await robot.gpioSet('pompe_d', False)
+
+@robot.sequence
+async def pumps_on():
+    await pump_on(True, True)
+
+@robot.sequence
+async def pumps_off():
+    await pump_off(True, True)
+
+@robot.sequence
+async def left_pump_on():
+    await pump_on(True, False)
+
+@robot.sequence
+async def left_pump_off():
+    await pump_off(True, False)
+
+@robot.sequence
+async def right_pump_on():
+    await pump_on(False, True)
+
+@robot.sequence
+async def right_pump_off():
+    await pump_off(False, True)
+
+
+arms_initialize_epaules_devant = {
+    'epaule_g': 2052,
+    'epaule_d': 2035
+}
+
+arms_initialize_epaules_rentrees = {
+    'epaule_g': 2952,
+    'epaule_d': 1131
+}
+
+arms_initialize_coudes_leves = {
+    'coude_g': 827,
+    'coude_d': 204
+}
+
+@robot.sequence
+async def arms_initialize():
+
+    # Activation dynamixels
+    await servos.setMaxTorque(['epaule_g', 'epaule_d'], 0.30)
+    await asyncio.sleep(0.1)
+    await servos.setMaxTorque(['coude_g', 'coude_d'], 0.20)
+    await asyncio.sleep(0.1)
+    
+    await servos.setEnable(['epaule_g', 'epaule_d'], True)
+    await asyncio.sleep(0.1)
+    await servos.setEnable(['coude_g', 'coude_d'], True)
+    await asyncio.sleep(0.1)
+    
+    # Init bras
+    await servos.moveMultiple(arms_initialize_epaules_devant, speed=0.7)
+    await asyncio.sleep(0.2)
+    await servos.moveMultiple(arms_initialize_coudes_leves, speed=0.7)
+    await asyncio.sleep(0.2)
+    
+    # initialisation ascenseurs    
+    await servos.liftDoHoming(0)
+    await servos.liftDoHoming(1)
+    await asyncio.sleep(4)
+    
+    await servos.liftsRaw(65, 60, 65, 60)
+    await asyncio.sleep(1)
+    await servos.moveMultiple(arms_initialize_epaules_rentrees, speed=0.7)
+    await asyncio.sleep(0.1)
+    await servos.setMaxTorque(['epaule_g', 'epaule_d'], 1)
+    await asyncio.sleep(0.1)
+    await servos.setMaxTorque(['coude_g', 'coude_d'], 1)
+    await asyncio.sleep(0.1)
+
+
+@robot.sequence
+async def arms_disable():
+    await servos.setMaxTorque(['epaule_g', 'epaule_d'], 0.0)
+    await asyncio.sleep(0.1)
+    await servos.setMaxTorque(['coude_g', 'coude_d'], 0.0)
+    await asyncio.sleep(0.1)
+    await servos.setEnable(['epaule_g', 'epaule_d'], False)
+    await asyncio.sleep(0.1)
+    await servos.setEnable(['coude_g', 'coude_d'], False)
+    await asyncio.sleep(0.1)
+    # WTF ?!!!!
+    await servos.moveMultiple(arms_initialize_epaules_devant, speed=0.7)
+    await asyncio.sleep(0.2)
+    await servos.moveMultiple(arms_initialize_coudes_leves, speed=0.7)
+    await asyncio.sleep(0.2)
+    
+
+@robot.sequence
+async def lifts_high():
+    await goldo_lifts_move(pos = 950, speed = 80)
+    await asyncio.sleep(0.4)
+
+
+arms_open_epaules_ouvertes = {
+    'epaule_g': 1609,
+    'epaule_d': 2277
+}
+
+arms_open_coudes_leves = {
+    'coude_g': 827,
+    'coude_d': 204
+}
+
+@robot.sequence
+async def arms_open():
+    await servos.moveMultiple(arms_open_epaules_ouvertes, speed=1)
+    await asyncio.sleep(0.1)
+    await servos.moveMultiple(arms_open_coudes_leves, speed=1)
+    await asyncio.sleep(0.1)
+
+
+arms_open_extreme_epaules_ouvertes = {
+    'epaule_g': 1090,
+    'epaule_d': 3000
+}
+
+arms_open_extreme_coudes_leves = {
+    'coude_g': 827,
+    'coude_d': 204
+}
+
+@robot.sequence
+async def arms_open_extreme():
+    await servos.moveMultiple(arms_open_extreme_epaules_ouvertes, speed=1)
+    await asyncio.sleep(0.1)
+    await servos.moveMultiple(arms_open_extreme_coudes_leves, speed=1)
+    await asyncio.sleep(0.1)
+
+
+arms_close_epaules_rentrees = {
+    'epaule_g': 2952,
+    'epaule_d': 1131
+}
+
+arms_close_coudes_leves = {
+    'coude_g': 827,
+    'coude_d': 204
+}
+
+@robot.sequence
+async def arms_close():
+    await servos.moveMultiple(arms_close_epaules_rentrees, speed=1)
+    await asyncio.sleep(0.1)
+    await servos.moveMultiple(arms_close_coudes_leves, speed=1)
+    await asyncio.sleep(0.1)
+
+
+position_defensive_laterale_epaules_ouvertes = {
+    'epaule_g': 1090,
+    'epaule_d': 3000
+}
+
+position_defensive_laterale_coudes_leves = {
+    'coude_g': 827,
+    'coude_d': 204
+}
+
+@robot.sequence
+async def position_defensive_laterale():
+    await goldo_lifts_move(pos = 800, speed = 80)
+    await asyncio.sleep(0.4)
+    await servos.moveMultiple(position_defensive_laterale_epaules_ouvertes, speed=1)
+    await asyncio.sleep(0.4)
+    await servos.moveMultiple(position_defensive_laterale_coudes_leves, speed=1)
+    await asyncio.sleep(0.4)
+
+
+position_defensive_laterale_servos_d = {
+    'epaule_d': 3000,
+    'coude_d': 204
+}
+
+@robot.sequence
+async def position_defensive_laterale_d():
+    await goldo_lift_move(GoldoLift.Right,800)
+    await asyncio.sleep(0.4)
+    await servos.moveMultiple(position_defensive_laterale_servos_d, speed=1)
+    await asyncio.sleep(0.4)
+
+
+position_defensive_laterale_servos_g = {
+    'epaule_g': 1090,
+    'coude_g': 827
+}
+
+@robot.sequence
+async def position_defensive_laterale_g():
+    await goldo_lift_move(GoldoLift.Left,800)
+    await asyncio.sleep(0.4)
+    await servos.moveMultiple(position_defensive_laterale_servos_g, speed=1)
+    await asyncio.sleep(0.4)
+
+
+bras_d_prep_take = {
+    'epaule_d': 1400,
+    'coude_d': 204,
+}
+
+@robot.sequence
+async def arm_right_prep_take():
+    await goldo_lift_move(GoldoLift.Right,320)
+    await asyncio.sleep(0.4)
+    await servos.moveMultiple(bras_d_prep_take, speed=1)
+    await asyncio.sleep(0.2)
+
+
+bras_d_take = {
+    'epaule_d': 1400,
+    'coude_d': 530,
+}
+
+@robot.sequence
+async def arm_right_take():
+    await servos.moveMultiple(bras_d_take, speed=1)
+    await asyncio.sleep(0.2)
+    await goldo_lift_move(GoldoLift.Right,60)
+    await asyncio.sleep(0.4)
+
+
+bras_g_prep_take = {
+    'epaule_g': 2740,
+    'coude_g': 824,
+}
+
+@robot.sequence
+async def arm_left_prep_take():
+    await goldo_lift_move(GoldoLift.Left,320)
+    await asyncio.sleep(0.4)
+    await servos.moveMultiple(bras_g_prep_take, speed=1)
+    await asyncio.sleep(0.2)
+
+
+bras_g_take = {
+    'epaule_g': 2740,
+    'coude_g': 497,
+}
+
+@robot.sequence
+async def arm_left_take():
+    await servos.moveMultiple(bras_g_take, speed=1)
+    await asyncio.sleep(0.2)
+    await goldo_lift_move(GoldoLift.Left,60)
+    await asyncio.sleep(0.4)
+
+
+bras_g_push_thermo = {
+#    'epaule_g': 2090,
+    'epaule_g': 2030,
+    'coude_g': 490,
+}
+
+@robot.sequence
+async def arm_left_push_thermo():
+    await goldo_lift_move(GoldoLift.Left,950)
+    await asyncio.sleep(0.4)
+    await servos.moveMultiple(bras_g_push_thermo, speed=0.5)
+    await asyncio.sleep(0.2)
+
+
+bras_d_push_thermo = {
+#    'epaule_d': 2090,
+    'epaule_d': 2150,
+    'coude_d': 490,
+}
+
+@robot.sequence
+async def arm_right_push_thermo():
+    await goldo_lift_move(GoldoLift.Right,950)
+    await asyncio.sleep(0.4)
+    await servos.moveMultiple(bras_d_push_thermo, speed=0.5)
+    await asyncio.sleep(0.2)
+
+
+
+## TESTS ##
+
+@robot.sequence
+async def test_grab_right():
+    await arm_right_prep_take()
+    await asyncio.sleep(0.5)
+    await right_pump_on()
+    await asyncio.sleep(0.1)
+    await arm_right_take()
+    await asyncio.sleep(0.5)
+    await position_defensive_laterale_d()
+    await asyncio.sleep(0.5)
+
+
+@robot.sequence
+async def test_grab_left():
+    await arm_left_prep_take()
+    await asyncio.sleep(0.5)
+    await left_pump_on()
+    await asyncio.sleep(0.1)
+    await arm_left_take()
+    await asyncio.sleep(0.5)
+    await position_defensive_laterale_g()
+    await asyncio.sleep(0.5)
+
+
+@robot.sequence
+async def test_crazy_grab():
+    # Propulsion
+    await odrive.clearErrors()
+    await propulsion.clearError()
+    await propulsion.setAccelerationLimits(1,1,2,2)
+    await propulsion.setMotorsEnable(True)
+    await propulsion.setEnable(True)
+    
+    await test_grab_right()
+    await asyncio.sleep(0.5)
+
+    await propulsion.translation(0.05, 0.2)
+    await asyncio.sleep(0.5)
+
+    await test_grab_left()
+    await asyncio.sleep(0.5)
+
+
+@robot.sequence
+async def goldo_lifts_test_initialize():
+    # initialisation ascenseurs    
+    await servos.liftDoHoming(0)
+    await servos.liftDoHoming(1)
+    await asyncio.sleep(6)
+    await servos.liftsRaw(65, 60, 65, 60)
+    await asyncio.sleep(3)
+
+
+@robot.sequence
+async def goldo_lifts_test_pos_0():
+    await goldo_lifts_move(0,25)
+
+@robot.sequence
+async def goldo_lifts_test_pos_65():
+    await goldo_lifts_move(65,25)
+
+@robot.sequence
+async def goldo_lifts_test_pos_200():
+    await goldo_lifts_move(200,25)
+
+@robot.sequence
+async def goldo_lifts_test_pos_250():
+    await goldo_lifts_move(400,25)
+
+@robot.sequence
+async def goldo_lifts_test_pos_600():
+    await goldo_lifts_move(600,25)
+
+@robot.sequence
+async def goldo_lifts_test_pos_800():
+    await goldo_lifts_move(800,25)
+
+@robot.sequence
+async def goldo_lifts_test_pos_1000():
+    await goldo_lifts_move(1000,25)
+
+
---
+++ config/coupe_be_2026/sequences/sequences.py
@@ -0,0 +1,456 @@
+# Sequences de la Coupe de France 2023 - gardees pour test & debug
+
+# import base modules
+import asyncio
+import numpy as np
+
+import time
+
+# NOTE : prise_offset_back_robot = 0.305
+# NOTE : robot_back_length       = 0.084
+# NOTE : prise_offset            = 0.220
+
+# import modules from sequence directory
+from . import positions as pos
+from . import recalages
+from . import actuators
+from . import robot_config as rc
+
+# objects included in the _sequences_globals of RobotMain class, defined in robot_main.py of goldo_main, are available as global variables
+# those objects are used to interact with the robot (send commands, read data)
+
+global_long_speed = 0.4
+global_turn_speed = 2.0
+
+aruco_start_configuration = "0000"
+
+@robot.sequence
+async def prematch():
+
+    global poses
+
+    if robot.side == 1: # YELLOW
+        poses = pos.YellowPoses
+    elif robot.side == 2: # BLUE
+        poses = pos.BluePoses
+    else:
+        raise RuntimeError('Side not set')
+
+    # Propulsion
+    await odrive.clearErrors()
+    await propulsion.clearError()
+    await propulsion.setAccelerationLimits(1,1,2,2)
+    await propulsion.setMotorsEnable(True)
+    await propulsion.setEnable(True)
+    
+    # Lidar
+    robot._adversary_detection_enable = False
+    #await lidar.start()
+
+    # Actionneurs
+    await actuators.arms_initialize()
+    await asyncio.sleep(1.0)
+
+    await actuators.arms_close()
+    await asyncio.sleep(0.5)
+
+    # Placement
+    await recalages.recalage()
+    await asyncio.sleep(1.0)
+
+    await actuators.position_defensive_laterale()
+    await asyncio.sleep(0.5)
+
+    # Dummy score
+    await robot.setScore(42)
+    print ("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+    print ("$ Dummy score 42")
+    print ("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+    await robot.gpioSet('keyboard_led', True)
+
+    return True
+
+
+@robot.sequence
+async def start_match():
+    """
+    Sequence called at the start of the match, before trying any action.
+    This will typically be used to setup actuators and get out of the starting area.
+    """
+
+    global aruco_start_configuration
+
+    T0 = time.time()
+
+    await propulsion.setAccelerationLimits(1,1,20,20)
+
+    T1 = time.time()
+    print ("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+    print ("T match_timer = {}".format(T1-T0))
+    print ("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+
+    #robot._adversary_detection_enable = True
+    await lidar.start()
+
+    try:
+        await strat_0()
+    except:
+        print ("EXCEPTION!")
+
+    await actuators.pumps_off()
+    await asyncio.sleep(0.5)
+    await actuators.arms_close()
+    await asyncio.sleep(0.5)
+
+    await propulsion.pointTo(poses.Final_escape_wp, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveToRetry(poses.Final_escape_wp, global_long_speed)
+    await asyncio.sleep(1.0)
+
+    await propulsion.pointTo(poses.Final_pose_wp, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveToRetry(poses.Final_pose_wp, global_long_speed)
+    await asyncio.sleep(1.0)
+
+    await lidar.stop()
+
+    T1 = time.time()
+    print ("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+    print ("T match_timer = {}".format(T1-T0))
+    print ("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+
+
+#reference x values:
+# 501 -> ref_x1 = 0.510
+# 562 -> ref_x2 = 0.560
+# 616 -> ref_x3 = 0.610
+# 671 -> ref_x4 = 0.660
+@robot.sequence
+async def aruco_start_detection():
+    global aruco_start_configuration
+
+    aruco_start_configuration = "0000"
+
+    ref_x = [0.510, 0.560, 0.610, 0.660]
+    segm_ref_x = [0.535, 0.585, 0.635]
+
+    if (robot.start_zone==1):   # BLUE   (Y+,id=36)
+        print("my_color = BLUE (id=36)")
+        my_color = 36
+    elif (robot.start_zone==2): # YELLOW (Y-,id=47)
+        print("my_color = YELLOW (id=47)")
+        my_color = 47
+    else:
+        print("No start zone!")
+        return
+
+    detections = []
+    try:
+        with open("/tmp/detections.txt") as f:
+            for line in f:
+                print ("DETECTIONS FILE: {}".format(line)) 
+                line = line.strip()
+                parts = line.split()
+                if len(parts) != 4:
+                    continue
+                ts_str, my_id_str, x_str, y_str = parts
+                ts = float(ts_str)
+                my_id = int(my_id_str)
+                x_real = float(x_str)/1000.0
+                y_real = float(y_str)/1000.0
+                detections.append((ts,my_id,x_real,y_real))
+    except:
+        print ("Cannot read (and parse) /tmp/detections.txt")
+        return
+
+    detections.sort(key=lambda d: d[2])
+
+    pres_idx = []
+
+    for i in range(len(detections)):
+        ts, my_id, x_real, y_real = detections[i]
+        print ("ts={:.2f} id={:d} x={:6.3f} y={:6.3f}".format(ts, my_id, x_real, y_real))
+        if (x_real<segm_ref_x[0]):
+            pres_idx.append(0)
+        elif (x_real<segm_ref_x[1]):
+            pres_idx.append(1)
+        elif (x_real<segm_ref_x[2]):
+            pres_idx.append(2)
+        else:
+            pres_idx.append(3)
+    print("Present: {}".format(pres_idx))
+
+    if (len(detections)<3 or len(detections)>4):
+        print("Cannot detect aruco_start_configuration!")
+        return
+
+    if (len(detections)==3):
+        miss_idx = None
+        miss_color_code = None
+        my_color_cnt = 0
+        adv_color_cnt = 0
+        for i in range(0,4):
+            if (i not in pres_idx):
+                miss_idx = i
+        for i in range(0,3):
+            if (detections[i][1]==my_color):
+                my_color_cnt = my_color_cnt + 1
+            else:
+                adv_color_cnt = adv_color_cnt + 1
+        print ("miss_idx={}".format(miss_idx))
+        print ("my_color_cnt={}".format(my_color_cnt))
+        print ("adv_color_cnt={}".format(adv_color_cnt))
+        if (my_color_cnt>adv_color_cnt):
+            miss_color_code = '0'
+        else:
+            miss_color_code = '1'
+        print ("miss_color_code={}".format(miss_color_code))
+        for i in range(0,3):
+            if (detections[i][1]==my_color):
+                aruco_start_configuration = aruco_start_configuration[:pres_idx[i]] + '1' + aruco_start_configuration[pres_idx[i]+1:]
+            print("  STEP {} : {}".format(i,aruco_start_configuration))
+        aruco_start_configuration = aruco_start_configuration[:miss_idx] + miss_color_code + aruco_start_configuration[miss_idx+1:]
+
+    elif (len(detections)==4):
+        for i in range(0,4):
+            if (detections[i][1]==my_color):
+                aruco_start_configuration = aruco_start_configuration[:i] + '1' + aruco_start_configuration[i+1:]
+
+    print("aruco_start_configuration = {}".format(aruco_start_configuration))
+
+
+async def do_push_0000():
+    await propulsion.moveTo(poses.First_push, global_long_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.First_grab_wp2, global_long_speed)
+    await asyncio.sleep(1.0)
+
+async def do_grab_and_push_0011():
+    await propulsion.moveTo(poses.First_grab_wp1, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.test_grab_right()
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.First_grab_wp2, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.test_grab_left()
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.First_push_out, global_long_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.pointTo(pt=poses.First_return_wp, yaw_rate=global_turn_speed, back=True)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.First_return_wp, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.pumps_off()
+    await asyncio.sleep(0.5)
+
+
+async def do_grab_and_push_0101():
+    await propulsion.moveTo(poses.First_grab_wp1, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.test_grab_right()
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.First_grab_wp3, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.test_grab_left()
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.First_push_out, global_long_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.pointTo(pt=poses.First_return_wp, yaw_rate=global_turn_speed, back=True)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.First_return_wp, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.pumps_off()
+    await asyncio.sleep(0.5)
+
+async def do_grab_and_push_0110():
+    await propulsion.moveTo(poses.First_grab_wp1, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.test_grab_right()
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.First_push_out, global_long_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.pointTo(pt=poses.First_return_wp, yaw_rate=global_turn_speed, back=True)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.First_return_wp, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.pumps_off()
+    await asyncio.sleep(0.5)
+
+async def do_grab_and_push_1001():
+    await propulsion.moveTo(poses.First_grab_wp2, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.test_grab_right()
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.First_grab_wp3, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.test_grab_left()
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.First_push_out, global_long_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.pointTo(pt=poses.First_return_wp, yaw_rate=global_turn_speed, back=True)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.First_return_wp, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.pumps_off()
+    await asyncio.sleep(0.5)
+
+async def do_grab_and_push_1010():
+    await propulsion.moveTo(poses.First_grab_wp2, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.test_grab_right()
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.First_push_out, global_long_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.pointTo(pt=poses.First_return_wp, yaw_rate=global_turn_speed, back=True)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.First_return_wp, global_long_speed)
+    await asyncio.sleep(1.0)
+    await actuators.pumps_off()
+    await asyncio.sleep(0.5)
+
+async def do_grab_and_push_1100():
+    await propulsion.moveTo(poses.First_push_out, global_long_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.First_grab_wp2, global_long_speed)
+    await asyncio.sleep(1.0)
+
+
+grab_and_push_funcs = {
+    "0011":do_grab_and_push_0011,
+    "0101":do_grab_and_push_0101,
+    "0110":do_grab_and_push_0110,
+    "1001":do_grab_and_push_1001,
+    "1010":do_grab_and_push_1010,
+    "1100":do_grab_and_push_1100,
+}
+
+@robot.sequence
+async def strat_0():
+    global aruco_start_configuration
+
+    try:
+        await aruco_start_detection()
+    except:
+        print ("aruco_start_detection() failed")
+        aruco_start_configuration = "0000"
+
+    if (aruco_start_configuration not in grab_and_push_funcs.keys()):
+        await do_push_0000()
+    else:
+        await grab_and_push_funcs[aruco_start_configuration]()
+
+    robot._adversary_detection_enable = True
+
+    await propulsion.pointTo(poses.Inter_wp1, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.Inter_wp1, global_long_speed)
+    await asyncio.sleep(1.0)
+
+    await propulsion.pointTo(poses.Inter_wp2, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.Inter_wp2, global_long_speed)
+    await asyncio.sleep(1.0)
+
+    await actuators.arms_close()
+    await asyncio.sleep(0.5)
+
+    await actuators.lifts_high()
+    await asyncio.sleep(0.5)
+
+    await propulsion.pointTo(poses.Corner_wp, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.Corner_wp, global_long_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.pointTo(pt=poses.Border_wp1, yaw_rate=global_turn_speed, back=True)
+    await asyncio.sleep(1.0)
+
+    print("CORNER!")
+
+    if (robot.start_zone==1):   # BLUE   (Y+)
+        await actuators.arm_right_push_thermo()
+        await asyncio.sleep(0.5)
+    elif (robot.start_zone==2): # YELLOW (Y-)
+        await actuators.arm_left_push_thermo()
+        await asyncio.sleep(0.5)
+    else:
+        print("No start zone!")
+
+    await asyncio.sleep(2.0)
+
+    await propulsion.moveTo(poses.Border_wp1, global_long_speed)
+    await asyncio.sleep(1.0)
+
+    await actuators.arms_close()
+    await asyncio.sleep(0.5)
+
+    #await propulsion.pointTo(poses.Final_escape_wp, global_turn_speed)
+    #await asyncio.sleep(1.0)
+    #await propulsion.moveTo(poses.Final_escape_wp, global_long_speed)
+    #await asyncio.sleep(1.0)
+    #await propulsion.pointTo(poses.Final_pose_wp, global_turn_speed)
+    #await asyncio.sleep(1.0)
+    #await propulsion.moveTo(poses.Final_pose_wp, global_long_speed)
+    #await asyncio.sleep(1.0)
+
+
+@robot.sequence
+async def test_dry_run():
+    await propulsion.moveTo(poses.TEST_wp1, global_long_speed)
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.TEST_wp2, global_long_speed)
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.TEST_wp3, global_long_speed)
+    await asyncio.sleep(0.5)
+    await propulsion.moveTo(poses.TEST_wp4, global_long_speed)
+    await asyncio.sleep(0.5)
+
+    await propulsion.moveTo(poses.TEST_wp5, global_long_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.TEST_wp6, global_long_speed)
+    await asyncio.sleep(1.0)
+
+    await propulsion.pointTo(poses.TEST_wp7, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.TEST_wp7, global_long_speed)
+    await asyncio.sleep(1.0)
+
+    await actuators.arms_close()
+    await asyncio.sleep(0.5)
+
+    await propulsion.pointTo(poses.TEST_wp8, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.TEST_wp8, global_long_speed)
+    await asyncio.sleep(1.0)
+
+    await propulsion.pointTo(poses.TEST_wp9, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.TEST_wp9, global_long_speed)
+    await asyncio.sleep(1.0)
+
+    await propulsion.pointTo(poses.TEST_wp10_N, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.TEST_wp10, global_long_speed)
+    await asyncio.sleep(10.0)
+
+    await propulsion.pointTo(poses.TEST_wp11, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.TEST_wp11, global_long_speed)
+    await asyncio.sleep(1.0)
+
+    await propulsion.pointTo(poses.TEST_wp12, global_turn_speed)
+    await asyncio.sleep(1.0)
+    await propulsion.moveTo(poses.TEST_wp12, global_long_speed)
+    await asyncio.sleep(1.0)
+
+
+@robot.sequence
+async def test_start_adversary_detection():
+    robot._adversary_detection_enable = False
+    await lidar.start()
+    robot._adversary_detection_enable = True
+
+@robot.sequence
+async def test_stop_adversary_detection():
+    robot._adversary_detection_enable = False
+    await lidar.stop()
+
