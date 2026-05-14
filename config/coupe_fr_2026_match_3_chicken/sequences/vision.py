3dec52de40d70375bc83b5a596de8f308a6fef02
Goldorak <echelon@free.fr>
2026-05-14 19:31:16 +0200

    * Strategies for matches 1, 2, 3 of the French cup

---
+++ config/coupe_fr_2026_match_3_chicken/sequences/vision.py
@@ -0,0 +1,245 @@
+import asyncio
+import numpy as np
+
+import time
+
+aruco_x_configuration = "0000"
+aruco_y_configuration = "0000"
+
+#reference x values:
+# 303 -> ref_x1 = 0.303
+# 358 -> ref_x2 = 0.358
+# 409 -> ref_x3 = 0.409
+# 466 -> ref_x4 = 0.466
+def aruco_x_detection():
+    global aruco_x_configuration
+
+    aruco_x_configuration = "0000"
+
+    ref_x = [0.303, 0.358, 0.409, 0.466]
+    segm_ref_x = [0.330, 0.383, 0.438]
+
+    if (robot.start_zone==1):   # BLUE   (Y+,id=36)
+        print("my_color = BLUE (id=36)")
+        my_color = 36
+    elif (robot.start_zone==2): # YELLOW (Y-,id=47)
+        print("my_color = YELLOW (id=47)")
+        my_color = 47
+    else:
+        print("No start zone!")
+        aruco_x_configuration = "0000"
+        return aruco_x_configuration
+
+    detections_file = "/tmp/detections.txt"
+    #detections_file = "/tmp/last_good_detections.txt"
+
+    detections = []
+    try:
+        with open(detections_file) as f:
+            for line in f:
+                print ("DETECTIONS FILE: {}".format(line)) 
+                line = line.strip()
+                parts = line.split()
+                if len(parts) != 4:
+                    continue
+                ts_str, my_id_str, x_str, y_str = parts
+                ts = float(ts_str)
+                my_id = int(my_id_str)
+                x_real = float(x_str)/1000.0
+                y_real = float(y_str)/1000.0
+                if (x_real>(ref_x[0]-0.050)) and (x_real<(ref_x[3]+0.050)) and (y_real>-0.030) and (y_real<0.030):
+                    detections.append((ts,my_id,x_real,y_real))
+    except:
+        print ("Cannot read (and parse) {}".format(detections_file))
+        aruco_x_configuration = "0000"
+        return aruco_x_configuration
+
+    detections.sort(key=lambda d: d[2])
+
+    pres_idx = []
+
+    for i in range(len(detections)):
+        ts, my_id, x_real, y_real = detections[i]
+        print ("ts={:.2f} id={:d} x={:6.3f} y={:6.3f}".format(ts, my_id, x_real, y_real))
+        if (x_real<segm_ref_x[0]):
+            pres_idx.append(0)
+        elif (x_real<segm_ref_x[1]):
+            pres_idx.append(1)
+        elif (x_real<segm_ref_x[2]):
+            pres_idx.append(2)
+        else:
+            pres_idx.append(3)
+    print("Present: {}".format(pres_idx))
+
+    if (len(detections)<3 or len(detections)>4):
+        print("len(detections)={}".format(len(detections)))
+        print("Cannot detect aruco_x_configuration!")
+        aruco_x_configuration = "0000"
+        return aruco_x_configuration
+
+    if (len(detections)==3):
+        miss_idx = None
+        miss_color_code = None
+        my_color_cnt = 0
+        adv_color_cnt = 0
+        for i in range(0,4):
+            if (i not in pres_idx):
+                miss_idx = i
+        for i in range(0,3):
+            if (detections[i][1]==my_color):
+                my_color_cnt = my_color_cnt + 1
+            else:
+                adv_color_cnt = adv_color_cnt + 1
+        print ("miss_idx={}".format(miss_idx))
+        print ("my_color_cnt={}".format(my_color_cnt))
+        print ("adv_color_cnt={}".format(adv_color_cnt))
+        if (my_color_cnt>adv_color_cnt):
+            miss_color_code = '0'
+        else:
+            miss_color_code = '1'
+        print ("miss_color_code={}".format(miss_color_code))
+        for i in range(0,3):
+            if (detections[i][1]==my_color):
+                aruco_x_configuration = aruco_x_configuration[:pres_idx[i]] + '1' + aruco_x_configuration[pres_idx[i]+1:]
+            print("  STEP {} : {}".format(i,aruco_x_configuration))
+        aruco_x_configuration = aruco_x_configuration[:miss_idx] + miss_color_code + aruco_x_configuration[miss_idx+1:]
+
+    elif (len(detections)==4):
+        for i in range(0,4):
+            if (detections[i][1]==my_color):
+                aruco_x_configuration = aruco_x_configuration[:i] + '1' + aruco_x_configuration[i+1:]
+
+    print("aruco_x_configuration = {}".format(aruco_x_configuration))
+
+    return aruco_x_configuration
+
+
+#reference x values: REF_X = 0.328
+# -80 -> ref_y1 = -0.079
+# -30 -> ref_y2 = -0.030
+#  30 -> ref_y3 =  0.030
+#  80 -> ref_y4 =  0.079
+def aruco_y_detection():
+    global aruco_y_configuration
+    REF_X = 0.328
+    x_shift = 0.0
+    y_shift = 0.0
+
+    aruco_y_configuration = "0000"
+
+    ref_y = [-0.080, -0.030, 0.030, 0.080]
+    segm_ref_y = [-0.055, 0.000, 0.055]
+
+    if (robot.start_zone==1):   # BLUE   (Y+,id=36)
+        print("my_color = BLUE (id=36)")
+        my_color = 36
+    elif (robot.start_zone==2): # YELLOW (Y-,id=47)
+        print("my_color = YELLOW (id=47)")
+        my_color = 47
+    else:
+        print("No start zone!")
+        aruco_y_configuration = "0000"
+        return aruco_y_configuration, 0.0, 0.0
+
+    detections_file = "/tmp/detections.txt"
+    #detections_file = "/tmp/last_good_detections.txt"
+
+    detections = []
+    try:
+        with open(detections_file) as f:
+            for line in f:
+                print ("DETECTIONS FILE: {}".format(line)) 
+                line = line.strip()
+                parts = line.split()
+                if len(parts) != 4:
+                    continue
+                ts_str, my_id_str, x_str, y_str = parts
+                ts = float(ts_str)
+                my_id = int(my_id_str)
+                x_real = float(x_str)/1000.0
+                y_real = float(y_str)/1000.0
+                if (x_real>(0.330-0.030)) and (x_real<(0.330+0.030)) and (y_real>(ref_y[0]-0.050)) and (y_real<(ref_y[3]+0.050)):
+                    detections.append((ts,my_id,x_real,y_real))
+    except:
+        print ("Cannot read (and parse) {}".format(detections_file))
+        aruco_y_configuration = "0000"
+        return aruco_y_configuration, 0.0, 0.0
+
+    detections.sort(key=lambda d: d[3])
+
+    pres_idx = []
+
+    for i in range(len(detections)):
+        ts, my_id, x_real, y_real = detections[i]
+        print ("ts={:.2f} id={:d} x={:6.3f} y={:6.3f}".format(ts, my_id, x_real, y_real))
+        if (y_real<segm_ref_y[0]):
+            pres_idx.append(0)
+        elif (y_real<segm_ref_y[1]):
+            pres_idx.append(1)
+        elif (y_real<segm_ref_y[2]):
+            pres_idx.append(2)
+        else:
+            pres_idx.append(3)
+    print("Present: {}".format(pres_idx))
+
+    if (len(detections)<3 or len(detections)>4):
+        print("len(detections)={}".format(len(detections)))
+        print("Cannot detect aruco_y_configuration!")
+        aruco_y_configuration = "0000"
+        return aruco_y_configuration, 0.0, 0.0
+
+    if (len(detections)==3):
+        miss_idx = None
+        miss_color_code = None
+        my_color_cnt = 0
+        adv_color_cnt = 0
+        for i in range(0,4):
+            if (i not in pres_idx):
+                miss_idx = i
+        for i in range(0,3):
+            if (detections[i][1]==my_color):
+                my_color_cnt = my_color_cnt + 1
+            else:
+                adv_color_cnt = adv_color_cnt + 1
+        print ("miss_idx={}".format(miss_idx))
+        print ("my_color_cnt={}".format(my_color_cnt))
+        print ("adv_color_cnt={}".format(adv_color_cnt))
+        if (my_color_cnt>adv_color_cnt):
+            miss_color_code = '0'
+        else:
+            miss_color_code = '1'
+        print ("miss_color_code={}".format(miss_color_code))
+        x_sum = 0.0
+        for i in range(0,3):
+            ts, my_id, x_real, y_real = detections[i]
+            x_sum += x_real
+            if (my_id==my_color):
+                aruco_y_configuration = aruco_y_configuration[:pres_idx[i]] + '1' + aruco_y_configuration[pres_idx[i]+1:]
+            print("  STEP {} : {}".format(i,aruco_y_configuration))
+        aruco_y_configuration = aruco_y_configuration[:miss_idx] + miss_color_code + aruco_y_configuration[miss_idx+1:]
+        x_shift = x_sum/3 - REF_X
+        if (miss_idx==1) or (miss_idx==2):
+            y_shift = (detections[0][3] + detections[2][3])/2
+        elif (miss_idx==0):
+            y_shift = (detections[0][3] + detections[1][3])/2
+        elif (miss_idx==3):
+            y_shift = (detections[1][3] + detections[2][3])/2
+
+    elif (len(detections)==4):
+        x_sum = 0.0
+        y_sum = 0.0
+        for i in range(0,4):
+            ts, my_id, x_real, y_real = detections[i]
+            x_sum += x_real
+            y_sum += y_real
+            if (my_id==my_color):
+                aruco_y_configuration = aruco_y_configuration[:i] + '1' + aruco_y_configuration[i+1:]
+        x_shift = x_sum/4 - REF_X
+        y_shift = y_sum/4
+
+    print("aruco_y_configuration = {}".format(aruco_y_configuration))
+    print("x_shift = {:6.3f}".format(x_shift))
+    print("y_shift = {:6.3f}".format(y_shift))
+
+    return aruco_y_configuration, x_shift, y_shift
+
---
+++ config/coupe_fr_2026_match_3_lion/sequences/positions.py
@@ -0,0 +1,292 @@
+import numpy as np
+#from . import robot_config as rc
+
+def symetrie(pose):
+    if isinstance(pose, np.ndarray):
+        if pose.shape[0] == 2:
+            return np.array([pose[0], -pose[1]])
+    return (pose[0], -pose[1], -pose[2])
+
+
+class Side:
+    Unknown = 0
+    Green = 1
+    Blue = 2
+
+
+# FIXME : TODO
+Resource = {
+    "R0-" : ( 0.800, -1.300,  -90.0),
+    "R1-" : ( 1.600, -1.300,  -90.0),
+    "R2-" : ( 1.800, -0.400,    0.0),
+    "R3-" : ( 1.200, -0.350, -180.0),
+    "R3+" : ( 1.200,  0.350, -180.0),
+    "R2+" : ( 1.800,  0.400,    0.0),
+    "R1+" : ( 1.600,  1.300,   90.0),
+    "R0+" : ( 0.800,  1.300,   90.0),
+    }
+
+# FIXME : TODO
+DropPoint = {
+    "D0-" : ( 1.200, -1.400,  -90.0),
+    "D1-" : ( 1.900, -0.800,    0.0),
+    "D2-" : ( 1.200, -0.700,    0.0),
+    "D3-" : ( 0.550, -0.250, -180.0),
+    "D4"  : ( 1.200,  0.000, -180.0),
+    "D5"  : ( 1.900,  0.000,    0.0),
+    "D3+" : ( 0.550,  0.250, -180.0),
+    "D2+" : ( 1.200,  0.700,    0.0),
+    "D1+" : ( 1.900,  0.800,    0.0),
+    "D0+" : ( 1.200,  1.400,   90.0),
+    }
+
+# FIXME : TODO
+DjWayPoint_preprise = {
+    -10 : ( 1.600, -1.085,  -90.0), # R1-
+    -20 : ( 1.585, -0.400,    0.0), # R2-
+    -30 : ( 0.985, -0.350,    0.0), # R3-
+    -31 : ( 1.415, -0.350, -180.0), # R3-
+     31 : ( 1.415,  0.350, -180.0), # R3+
+     30 : ( 0.985,  0.350,    0.0), # R3+
+     20 : ( 1.585,  0.400,    0.0), # R2+
+     10 : ( 1.600,  1.085,   90.0), # R1+
+    }
+
+# FIXME : TODO
+DjWayPoint_predepose = {
+      1 : ( 0.850,  0.000, -180.0),
+      2 : ( 0.935,  0.000,    0.0), # D4
+      3 : ( 1.465,  0.000, -180.0), # D4
+      4 : ( 1.635,  0.000,    0.0), # D5
+   -100 : ( 1.200, -1.135,  -90.0), # D0-
+   -110 : ( 1.635, -0.800,    0.0), # D1-
+   -120 : ( 1.465, -0.700, -180.0), # D2-
+   -121 : ( 0.935, -0.700,    0.0), # D2-
+    121 : ( 0.935,  0.700,    0.0), # D2+
+    120 : ( 1.465,  0.700, -180.0), # D2+
+    110 : ( 1.635,  0.800,    0.0), # D1+
+    100 : ( 1.200,  1.135,   90.0), # D0+
+    }
+
+# FIXME : TODO
+DjWayPoint_start = {
+   -200 : ( 0.320, -1.320,    0.0),
+    200 : ( 0.320,  1.320,    0.0),
+    }
+
+# FIXME : TODO
+DjWayPoint_extra = {
+   -300 : ( 0.500, -1.075,    0.0),
+   -310 : ( 0.800, -1.075,    0.0),
+   -320 : ( 1.720, -1.075,    0.0),
+   -330 : ( 0.960, -1.000,    0.0),
+   -340 : ( 0.700, -1.300,    0.0),
+    340 : ( 0.700,  1.300,    0.0),
+    330 : ( 0.960,  1.000,    0.0),
+    320 : ( 1.720,  1.075,    0.0),
+    310 : ( 0.800,  1.075,    0.0),
+    300 : ( 0.500,  1.075,    0.0),
+    }
+
+DjWayPoint = {}
+DjWayPoint.update (DjWayPoint_preprise)
+DjWayPoint.update (DjWayPoint_predepose)
+DjWayPoint.update (DjWayPoint_start)
+DjWayPoint.update (DjWayPoint_extra)
+
+# FIXME : TODO
+DjWayPointNet = [
+    (    1, -121),
+    (    1, -300),
+    (    1, -310),
+    (    2, -121),
+    (    2,  -30),
+    (    3,  -20),
+    (    4,  -20),
+    (    3,  -31),
+    (    4,  -31),
+    (  -30,  -31),
+    (  -20,  -31),
+    (  -30, -121),
+    ( -330, -121),
+    ( -120,  -31),
+    ( -110,  -20),
+    ( -120,  -20),
+    ( -110,  -31),
+    ( -110, -120),
+    (  -10, -110),
+    ( -100, -120),
+    (  -10, -100),
+    (  -10, -120),
+    ( -100, -110),
+    ( -300, -310),
+    ( -310, -330),
+    ( -330, -100),
+    ( -330, -110),
+    ( -340, -100),
+    ( -340, -310),
+    ( -340, -300),
+    ( -200, -300),
+    ( -200, -340),
+    ( -310, -100),
+    ( -330, -340),
+
+    (   30,  -30),
+    (    1,    2),
+    (   31,  -31),
+
+    (  330,  340),
+    (  310,  100),
+    (  200,  340),
+    (  200,  300),
+    (  340,  300),
+    (  340,  310),
+    (  340,  100),
+    (  330,  110),
+    (  330,  100),
+    (  310,  330),
+    (  300,  310),
+    (  100,  110),
+    (   10,  120),
+    (   10,  100),
+    (  100,  120),
+    (   10,  110),
+    (  110,  120),
+    (  110,   31),
+    (  120,   20),
+    (  110,   20),
+    (  120,   31),
+    (  330,  121),
+    (   30,  121),
+    (   20,   31),
+    (   30,   31),
+    (    4,   31),
+    (    3,   31),
+    (    4,   20),
+    (    3,   20),
+    (    2,   30),
+    (    2,  121),
+    (    1,  310),
+    (    1,  300),
+    (    1,  121),
+    ]
+
+# robot offset : 82 mm
+
+# BLUE : ZONE 1
+class BluePoses:
+    # Poses de depart
+    Start_pose       = (  0.320,  1.320,   0)
+
+    # REAL
+    First_grab_wp1   = (  0.510,  1.320,   0)
+    First_grab_wp2   = (  0.560,  1.320,   0)
+    First_grab_wp3   = (  0.610,  1.320,   0)
+    First_grab_wp4   = (  0.660,  1.320,   0)
+
+    First_push       = (  0.980,  1.320,   0)
+    First_push_out   = (  1.100,  1.320,   0)
+
+    #First_return_wp  = (  0.200,  1.200,   0)
+    #First_return_wp  = (  0.700,  1.300,   0)
+    First_return_wp  = (  0.600,  1.300,   0)
+
+    Inter_wp0        = (  0.500,  1.075,   0)
+    Inter_wp1        = (  0.800,  1.075,   0)
+    Inter_wp2        = (  1.720,  1.075,   0)
+    Inter_test_wp    = (  1.100,  0.800,   0)
+
+    Corner_wp        = (  1.850,  1.200, -60)
+
+    #Border_wp1       = (  1.850,  0.630, -90)
+    Border_wp1       = (  1.850,  0.650, -90)
+    Border_wp1_N     = (  1.850,  1.500, -90)
+
+    #Action1_grab_wp1 = (  1.600,  1.084,  90)
+    Action1_grab_wp1 = (  1.605,  1.084,  90)
+    Action1_drop_wp1 = (  1.634,  0.800,   0)
+
+    Action2_grab_wp0 = (  1.550,  0.400,   0)
+    #Action2_grab_wp1 = (  1.584,  0.400,   0)
+    Action2_grab_wp1 = (  1.594,  0.400,   0)
+    Action2_drop_wp1 = (  1.441,  0.000,-180)
+
+    Action42_grab_wp0 = (  1.480, -0.350,-180)
+    Action42_grab_wp1 = (  1.451, -0.350,-180)
+    Action42_drop_wp0 = (  1.100, -0.350,-180)
+    Action42_drop_wp1 = (  0.850,  0.000,-180)
+
+    #Action3_grab_wp0 = (  1.480,  0.350,-180)
+    ##Action3_grab_wp1 = (  1.441,  0.350,-180)
+    #Action3_grab_wp1 = (  1.460,  0.350,-180)
+    #Action3_drop_wp1 = (  1.440,  0.700,-180)
+
+    Action3_grab_wp0 = (  0.940,  0.350,   0)
+    Action3_grab_wp1 = (  0.960,  0.350,   0)
+    Action3_drop_wp1 = (  0.960,  0.700,   0)
+
+    #Final_escape_wp0 = (  1.440,  1.000,-180)
+    Final_escape_wp0 = (  0.960,  1.000,-180)
+    Final_escape_wp1 = (  0.700,  1.300,-180)
+    Final_pose_wp    = (  0.300,  1.300,-180)
+
+
+# YELLOW : ZONE 2
+class YellowPoses:
+    # Poses de depart
+    Start_pose       = symetrie(BluePoses.Start_pose)
+
+    # REAL
+    First_grab_wp1   = symetrie(BluePoses.First_grab_wp1)
+    First_grab_wp2   = symetrie(BluePoses.First_grab_wp2)
+    First_grab_wp3   = symetrie(BluePoses.First_grab_wp3)
+    First_grab_wp4   = symetrie(BluePoses.First_grab_wp4)
+
+    First_push       = symetrie(BluePoses.First_push)
+    First_push_out   = symetrie(BluePoses.First_push_out)
+
+    First_return_wp  = symetrie(BluePoses.First_return_wp)
+
+    Inter_wp0        = symetrie(BluePoses.Inter_wp0)
+    Inter_wp1        = symetrie(BluePoses.Inter_wp1)
+    Inter_wp2        = symetrie(BluePoses.Inter_wp2)
+    Inter_test_wp    = symetrie(BluePoses.Inter_test_wp)
+
+    Corner_wp        = symetrie(BluePoses.Corner_wp)
+
+    #Border_wp1       = symetrie(BluePoses.Border_wp1)
+    Border_wp1       = (  1.850, -0.640, -90)
+    Border_wp1_N     = symetrie(BluePoses.Border_wp1_N)
+
+    Action1_grab_wp1 = symetrie(BluePoses.Action1_grab_wp1)
+    Action1_drop_wp1 = symetrie(BluePoses.Action1_drop_wp1)
+
+    Action2_grab_wp0 = symetrie(BluePoses.Action2_grab_wp0)
+    Action2_grab_wp1 = symetrie(BluePoses.Action2_grab_wp1)
+    Action2_drop_wp1 = symetrie(BluePoses.Action2_drop_wp1)
+
+    Action42_grab_wp0 = symetrie(BluePoses.Action42_grab_wp0)
+    Action42_grab_wp1 = symetrie(BluePoses.Action42_grab_wp1)
+    Action42_drop_wp0 = symetrie(BluePoses.Action42_drop_wp0)
+    Action42_drop_wp1 = symetrie(BluePoses.Action42_drop_wp1)
+
+    Action3_grab_wp0 = symetrie(BluePoses.Action3_grab_wp0)
+    Action3_grab_wp1 = symetrie(BluePoses.Action3_grab_wp1)
+    Action3_drop_wp1 = symetrie(BluePoses.Action3_drop_wp1)
+
+    Final_escape_wp0 = symetrie(BluePoses.Final_escape_wp0)
+    Final_escape_wp1 = symetrie(BluePoses.Final_escape_wp1)
+    Final_pose_wp    = symetrie(BluePoses.Final_pose_wp)
+
+
+@robot.sequence
+async def print_start_zone():
+    print("Start zone : " + str(robot.start_zone))
+
+@robot.sequence
+async def led_off():
+    await robot.gpioSet('keyboard_led', False)
+
+@robot.sequence
+async def led_on():
+    await robot.gpioSet('keyboard_led', True)
